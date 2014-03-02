ded293cda32d7cc2f43dc227b3e97419341d807a
Felix Jones <fewix3000@hotmail.co.uk>
2014-03-02 16:07:06 +0000

    Added comments to the SmartPointer template class

--- include/SmartPointer.h
+++ include/SmartPointer.h
@@ -43,24 +43,28 @@ protected:
 */
 template<class T> class xiPtr {
 public:
+	// Smart pointer construct with pointer
 	xiPtr( T* ptr = nullptr ) : pointer( ptr ) {
 		if ( ptr != nullptr ) {
 			ptr->Grab();
 		}
 	}
 	
+	// Smart pointer copy constructor, will call ::Grab()
 	xiPtr( const xiPtr &ptr ) : pointer( ptr.pointer ) {
 		if( pointer != nullptr ) {
 			pointer->Grab();
 		}
 	}
 
+	// Smart pointer destructor, calls ::Drop() on xiPtr
 	~xiPtr() {
 		if( pointer != nullptr ) {
 			pointer->Drop();
 		}
 	}
-
+	
+	// Smart pointer assignment, will call ::Grab()
 	xiPtr &operator=( T* ptr ) {
 		if ( ptr != nullptr ) {
 			ptr->Grab();
@@ -73,35 +77,43 @@ public:
 		pointer = ptr;
 		return ( *this );
 	}
-
+	
+	// Smart pointer assignment, will call pointer assignment
 	xiPtr &operator=( const xiPtr &ptr ) {
 		return ( *this ) = ptr.pointer;
 	}
-
+	
+	// Smart pointer access
 	T* operator->() const {
 		return pointer;
 	}
-
+	
+	// Smart pointer dereference set
 	T &operator*() const {
 		return *pointer;
 	}
-
+	
+	// Smart pointer dereference
 	operator T*() const {
 		return pointer;
 	}
-
+	
+	// Smart pointer nullptr check
 	operator bool() const {
 		return pointer != nullptr;
 	}
-
+	
+	// Smart pointer compare with other
 	bool operator==(const xiPtr &ptr) {
 		return pointer == ptr.pointer;
 	}
-
+	
+	// Smart pointer compare with other pointer
 	bool operator==(const T *ptr) {
 		return pointer == ptr;
 	}
-
+	
+	// Smart pointer array accessor
 	T& operator[] ( int x ) {
 		return pointer[x];
 	}
