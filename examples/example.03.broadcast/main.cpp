213c2f70bfc44a8f08d700e240554acc2569458b
Felix Jones <fewix3000@hotmail.co.uk>
2014-03-16 18:40:46 +0000

    Replaced fseek on example clients as it does not work on Linux

--- examples/example.03.broadcast/main.cpp
+++ examples/example.03.broadcast/main.cpp
@@ -45,11 +45,12 @@ int main( int argc, char ** argv ) {
 		}
 		printf( "> " ); // Show the text input indicator
 
-#if defined( __X64__ ) && defined( __WIN_API__ )
-		_fseeki64( stdin, 0, SEEK_END ); // Clear the STDIN buffer
-#elif defined( __X32__ ) || !defined( __WIN_API__ )
-		fseek( stdin, 0, SEEK_END ); // Clear the STDIN buffer
+#if defined( __WIN_API__ )
+		fflush( stdin );
+#elif defined( __POSIX__ )
+		fpurge( stdin );
 #endif
+
 		const int scanState = scanf( BUFFER_STR, buffer ); // See BUFFER_STR above
 		// BUFFER_STR is set to allow text, numbers, spaces and colons
 		// Scanf will block while the user inputs text into buffer
